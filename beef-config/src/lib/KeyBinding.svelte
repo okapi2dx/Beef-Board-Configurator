c6d2b5feef897062cd9d5da2535b101e3da39395
okapi2dx <188749218+okapi2dx@users.noreply.github.com>
2026-09-19 07:16:16 +0900

    Restore full key and button mapping reset

--- beef-config/src/lib/KeyBinding.svelte
+++ beef-config/src/lib/KeyBinding.svelte
@@ -65,6 +65,7 @@
 
 	function resetKeys() {
 		config.iidx_keys = new IIDXKeyMapping();
+		config.button_mapping = BUTTON_LAYOUT_LABELS.map((_, i) => i);
 		selectedButton = null;
 		window.removeEventListener('keydown', handleKeyDown);
 		resetOpen = false;
