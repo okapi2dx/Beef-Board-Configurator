56eae70809c8296477abf4e280816085c3e754c5
okapi2dx <188749218+okapi2dx@users.noreply.github.com>
2026-09-19 10:54:26 +0900

    Expose DFU reconnect to renderer

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('beefNative', {
   selectFirmware: () => ipcRenderer.invoke('beef:select-firmware'),
   flashFirmware: () => ipcRenderer.invoke('beef:flash-firmware'),
+  restartDfu: () => ipcRenderer.invoke('beef:restart-dfu'),
   getVersion: () => ipcRenderer.invoke('beef:get-version'),
   checkForUpdates: () => ipcRenderer.invoke('beef:check-update'),
   openUpdate: url => ipcRenderer.invoke('beef:open-update', url),
